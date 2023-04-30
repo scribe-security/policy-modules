3af58b45387d61fea6d6fecdf65fb5ef45b90654
houdini91 <mdstrauss91@gmail.com>
2023-04-30 13:52:01 +0300

    tesT

--- modules/example.rego
+++ modules/example.rego
@@ -1,4 +1,4 @@
-package example
+package policies.example
 
 default allow = false
 
--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bundle=`ls -t dist/ | head -n 1`
 echo "Bundle: ${bundle}"
-GOGC=1000 opa eval -b dist/${bundle} "data.example.Verify" --input test/input.json --profile --format=pretty
+GOGC=1000 opa eval -b dist/${bundle} "data.policies.example.Verify" --input test/input.json --profile --format=pretty
